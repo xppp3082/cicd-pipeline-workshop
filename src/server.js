41a5680d0609c9f4e118c0e294ddb824bc6343ac
Travis Liu <xppp3082@gmail.com>
2024-07-24 11:33:07 +0800

    fix: modify server.js

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 const DB_PWD = process.env.DB_PWD;
 
 app.get('/', (req, res) => {
-  res.send('Hello Pipeline 2245');
+  res.send('Hello Pipeline 3345');
 });
 
 app.get('/db', (req, res) => {
